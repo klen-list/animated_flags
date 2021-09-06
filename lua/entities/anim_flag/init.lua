370ed350b5a4139cb816b51f7cd712a0fab202df
Klen_list <anton.burkov.02@mail.ru>
2021-09-07 02:21:25 +0300

    AutoMaterial Update!
    + New flags can be added by pushing new vtf's into materials\models\anim_flag_skins folder
    + Vtf's names configure flag name in menu
    + Reduction size of current flag materials
    + Renamed model and set error as default flag material (if no flags in material dir founded)

--- lua/entities/anim_flag/init.lua
+++ lua/entities/anim_flag/init.lua
@@ -4,8 +4,43 @@
 AddCSLuaFile"shared.lua"
 include"shared.lua"
 
+do
+	local skinc, mat = 0
+	MsgN"[Animated Flags] Starting material loading..."
+	file.CreateDir"animflag_loadtemp"
+	for i,matfile in ipairs(file.Find("materials/models/anim_flag_skins/*.vtf", "GAME")) do
+		if matfile == "flag_structure.vtf" then continue end -- model material, not flag
+		matfile = matfile:sub(1, #matfile - 4)
+
+		file.Write("animflag_loadtemp/" .. matfile .. ".vmt", Format("vertexlitgeneric{$basetexture \"models/anim_flag_skins/%s\"}", matfile))
+		mat = Material("../data/animflag_loadtemp/" .. matfile)
+
+		if mat:IsError() then
+			MsgN("[Animated Flags] Refused from loading error material `" .. matfile .. "`!")
+			continue
+		end
+
+		skinc = skinc + 1
+		SetGlobalString("animflag_skin" .. skinc, mat:GetName())
+
+		MsgN("[Animated Flags] Created material for `", matfile, "`...")
+	end
+	SetGlobalInt("animflag_skincount", skinc)
+end
+
+hook.Add("ShutDown", "AnimFlagClearDataTemp", function()
+	MsgN"[Animated Flags] Clearing vmt cache..."
+	for i,matfile in ipairs(file.Find("animflag_loadtemp/*.vmt", "DATA")) do
+		file.Delete("animflag_loadtemp/" .. matfile)
+	end
+	file.Delete"animflag_loadtemp"
+end)
+
 function ENT:Initialize()
-	self:SetModel"models/anim_flags/anim_flag.mdl"
+	self:SetModel"models/anim_flag_rework/anim_flag_rework.mdl"
+	if GetGlobalInt"animflag_skincount" > 0 then
+		self:SetSubMaterial(0, GetGlobalString"animflag_skin1")
+	end
 	self:SetMoveType(MOVETYPE_VPHYSICS)
 	self:PhysicsInit(SOLID_VPHYSICS)
 	self:PhysWake()
--- lua/entities/anim_flag/shared.lua
+++ lua/entities/anim_flag/shared.lua
@@ -2,47 +2,19 @@
 -- Remake Wild Russain x Klen_list
 
 ENT.Base = "base_anim"
-
 ENT.PrintName = "Animated Flag"
 ENT.Category = "Animated Flags"
-
 ENT.Spawnable = true
-
 ENT.AutomaticFrameAdvance = true
 
 if CLIENT then
-	function ENT:Draw()
-		self:DrawModel()
-	end
-	language.Add("anim_flag", "Animated Flag")
-	
-	ENT.Skins = {
-		"USA",
-		"Bombass",
-		"Deutschland Reich",
-		"United Kingdom",
-		"Error",
-		"Germany (1WW)",
-		"Gmod",
-		"LGBT",
-		"Russian Federation",
-		"SCP Foundation",
-		"Steam",
-		"Ukraine",
-		"USSR",
-		"Airborne Forces RF"
-	}
-	
 	if GetConVar"cl_language":GetString() == "russian" or GetConVar"gmod_language":GetString() == "ru" then
 		language.Add("flagchangeskin", "Скин флага")
 	else
 		language.Add("flagchangeskin", "Flag skin")
 	end
+	language.Add("anim_flag", "Animated Flag")
 end
-
-hook.Add("CanProperty", "AnimFlagsNoDefaultSkinProperty", function(ply, property, ent)
-	if (IsValid(ent) and ent:GetClass() == "anim_flag") and property == "skin" then return false end
-end)
 
 properties.Add("flagchangeskin", {
 	MenuLabel = "#flagchangeskin",
@@ -51,24 +23,34 @@ properties.Add("flagchangeskin", {
 
 	Filter = function(self, ent, ply)
 		if not (IsValid(ent) and ent:GetClass() == "anim_flag") then return false end
+		if GetGlobalInt"animflag_skincount" <= 0 then return false end
 		if not gamemode.Call("CanProperty", ply, "flagchangeskin", ent) then return false end
 
 		return true
 	end,
-	Action = function(self, ent)
-		-- Using SetSkin instead
+	ParseName = function(self, skinidx)
+		local name = string.Replace(string.GetFileFromFilename(GetGlobalString("animflag_skin" .. skinidx)), "_", " ")
+
+		local toup, out = true, ""
+		for i = 1,#name do
+			if name:sub(i,i) == "^" then toup = true continue end
+			if toup then out = out .. name:sub(i,i):upper() toup = false continue end
+			out = out .. name:sub(i,i)
+		end
+
+		return out
 	end,
-	MenuOpen = function(self, option, ent, tr)
+	MenuOpen = function(self, option, ent)
 		local submenu = option:AddSubMenu()
-		for i,skin in ipairs(ent.Skins) do
-			local option = submenu:AddOption(skin, function() self:SetSkin(ent, i - 1) end)
-			if ent:GetSkin() == i - 1 then option:SetChecked(true) end
+		for i = 1,GetGlobalInt"animflag_skincount" do
+			option = submenu:AddOption(self:ParseName(i), function() self:SendSkin(ent, i) end)
+			if ent:GetSubMaterial(0) == GetGlobalString("animflag_skin" .. i) then option:SetChecked(true) end
 		end
 	end,
-	SetSkin = function(self, ent, id)
+	SendSkin = function(self, ent, id)
 		self:MsgStart()
 			net.WriteEntity(ent)
-			net.WriteUInt(id, 8)
+			net.WriteUInt(id, 8) -- 8 bit = 255 skins, u cant reach this
 		self:MsgEnd()
 	end,
 	Receive = function(self, l, ply)
@@ -76,9 +58,9 @@ properties.Add("flagchangeskin", {
 
 		if not properties.CanBeTargeted(ent, ply) then return end
 		if not self:Filter(ent, ply) then return end
-		
-		local skin = net.ReadUInt(8)
 
-		ent:SetSkin(skin < 0 and 0 or skin > 13 and 13 or skin)
-	end 
+		local _skin = net.ReadUInt(8)
+
+		ent:SetSubMaterial(0, GetGlobalString("animflag_skin" .. (_skin < GetGlobalInt"animflag_skincount" and _skin or GetGlobalInt"animflag_skincount")))
+	end
 })
